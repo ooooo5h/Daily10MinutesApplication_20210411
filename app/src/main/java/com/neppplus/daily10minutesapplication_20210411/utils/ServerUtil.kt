c7ec2a50b2a40110f33c18bb45c4ceca16ab15ba
전은형 <ooooo5h91@gmail.com>
2021-04-11 13:57:30 +0900

    서버가 응답하는 내용을 JSONObject로 변환해서 로그로만 직어보자

--- app/src/main/java/com/neppplus/daily10minutesapplication_20210411/utils/ServerUtil.kt
+++ app/src/main/java/com/neppplus/daily10minutesapplication_20210411/utils/ServerUtil.kt
@@ -1,8 +1,9 @@
 package com.neppplus.daily10minutesapplication_20210411.utils
 
-import okhttp3.FormBody
-import okhttp3.OkHttpClient
-import okhttp3.Request
+import android.util.Log
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
 
 class ServerUtil {
 
@@ -27,7 +28,25 @@ class ServerUtil {
 
             val client = OkHttpClient()
 
-            client.newCall(request)
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+
+                    val bodyString = response.body!!.string()
+
+                    val jsonObj = JSONObject(bodyString)
+
+                    Log.d("서버응답", jsonObj.toString())
+
+
+                }
+
+
+            })
+
 
 
         }
