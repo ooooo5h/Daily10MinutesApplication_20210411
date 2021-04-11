2df700f47b5e8442d1305b4c686ec8ea94bc4819
전은형 <ooooo5h91@gmail.com>
2021-04-11 15:10:07 +0900

    회원가입 실제 실행 => 서버 응답까지 진행

--- app/src/main/java/com/neppplus/daily10minutesapplication_20210411/SignUpActivity.kt
+++ app/src/main/java/com/neppplus/daily10minutesapplication_20210411/SignUpActivity.kt
@@ -2,7 +2,10 @@ package com.neppplus.daily10minutesapplication_20210411
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.neppplus.daily10minutesapplication_20210411.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_sign_up.*
+import org.json.JSONObject
 
 class SignUpActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,6 +22,33 @@ class SignUpActivity : BaseActivity() {
             val inputEmail = emailEdt.text.toString()
             val inputPw = passwordEdt.text.toString()
             val inputNickname = nicknameEdt.text.toString()
+
+            ServerUtil.putRequestSingUp(inputEmail, inputPw, inputNickname, object : ServerUtil.JsonResponseHandler {
+                override fun onResponse(jsonObj: JSONObject) {
+
+                    val code = jsonObj.getInt("code")
+
+                    if (code == 200) {
+
+                        runOnUiThread {
+                            Toast.makeText(mContext, "환영합니다", Toast.LENGTH_SHORT).show()
+                            finish()
+                        }
+
+
+                    }
+                    else {
+                        val message = jsonObj.getString("message")
+                        runOnUiThread {
+                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                        }
+
+                    }
+
+                }
+
+
+            })
 
         }
 
