0367dcc0125be4d477cbd5c205e7aff59cbe54b8
전은형 <ooooo5h91@gmail.com>
2021-04-11 13:30:32 +0900

    로그인 API 호출 기능 작성

--- app/src/main/java/com/neppplus/daily10minutesapplication_20210411/utils/ServerUtil.kt
+++ app/src/main/java/com/neppplus/daily10minutesapplication_20210411/utils/ServerUtil.kt
@@ -1,5 +1,9 @@
 package com.neppplus.daily10minutesapplication_20210411.utils
 
+import okhttp3.FormBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
 class ServerUtil {
 
 
@@ -9,6 +13,21 @@ class ServerUtil {
 
         fun postRequestLogin(email : String, pw : String) {
 
+            val urlString = "${HOST_URL}/user"
+
+            val formData = FormBody.Builder()
+                .add("email", email)
+                .add("password", pw)
+                .build()
+
+            val request = Request.Builder()
+                .url(urlString) // 어디로 가는지?
+                .post(formData) // POST방식 - 필요데이터(formData) 들고 가도록
+                .build()
+
+            val client = OkHttpClient()
+
+            client.newCall(request)
 
 
         }
